506ed260cea4d66b1d52f40d667afdab3621422e
Vu Anh <anhv.ict91@gmail.com>
2018-12-06 11:12:09 +0700

    update data

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,13 +1,7 @@
-import os
-from os.path import dirname, exists
-from os.path import join
-
 from dictionary import Word, Dictionary
 
-DATA_PATH = join("hongocduc", "raw", "VV")
-lines = open(join(DATA_PATH, "vv30K.index")).read().splitlines()[4:]
+texts = open("data/Viet74K.txt").read().splitlines()
 
-texts = [line.split("\t")[0] for line in lines]
 tmp = []
 for text in texts:
     text = Word(text)
